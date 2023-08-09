d8ca47ea82e5ee27ff0c023233f2a91b74a21206
Mahdi Rahmani <mahdirahmani8@yahoo.com>
2023-08-09 10:00:20 +0330

    add font, and fix type

--- app/src/main/java/mx/ma3r/tokyogoul/ui/theme/Type.kt
+++ app/src/main/java/mx/ma3r/tokyogoul/ui/theme/Type.kt
@@ -2,33 +2,98 @@ package mx.ma3r.tokyogoul.ui.theme
 
 import androidx.compose.material3.Typography
 import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.Font
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDirection
 import androidx.compose.ui.unit.sp
+import mx.ma3r.tokyogoul.R
+
+val fonts = FontFamily(
+    Font(R.font.yekan),
+    Font(R.font.yekan_bold)
+)
 
 // Set of Material typography styles to start with
 val Typography = Typography(
+
     bodyLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
         fontSize = 16.sp,
         lineHeight = 24.sp,
-        letterSpacing = 0.5.sp
-    )
-    /* Other default text styles to override
-    titleLarge = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Normal,
-        fontSize = 22.sp,
-        lineHeight = 28.sp,
-        letterSpacing = 0.sp
+        letterSpacing = 0.5.sp,
+        textDirection = TextDirection.Rtl,
+    ),
+    bodyMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Medium,
+        fontSize = 14.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    bodySmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+        textDirection = TextDirection.Rtl
+
+    ),
+
+    headlineLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 60.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    headlineMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 34.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    headlineSmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 24.sp,
+        textDirection = TextDirection.Rtl
+    ),
+
+    labelLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 14.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    labelMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 12.sp,
+        textDirection = TextDirection.Rtl
     ),
     labelSmall = TextStyle(
-        fontFamily = FontFamily.Default,
-        fontWeight = FontWeight.Medium,
-        fontSize = 11.sp,
-        lineHeight = 16.sp,
-        letterSpacing = 0.5.sp
+        fontFamily = fonts,
+        fontWeight = FontWeight.Light,
+        fontSize = 10.sp,
+        textDirection = TextDirection.Rtl
+    ),
+
+    titleLarge = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 18.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    titleMedium = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 16.sp,
+        textDirection = TextDirection.Rtl
+    ),
+    titleSmall = TextStyle(
+        fontFamily = fonts,
+        fontWeight = FontWeight.Bold,
+        fontSize = 14.sp,
+        textDirection = TextDirection.Rtl
+    ),
+
     )
-    */
-)
