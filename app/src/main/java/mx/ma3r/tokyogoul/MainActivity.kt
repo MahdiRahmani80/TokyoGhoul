dcb742d18081d5e7edce84facf6f506ef8440273
Mahdi Rahmani <mahdirahmani8@yahoo.com>
2023-08-09 09:33:52 +0330

    initial commit

---
+++ app/src/main/java/mx/ma3r/tokyogoul/MainActivity.kt
@@ -0,0 +1,40 @@
+package mx.ma3r.tokyogoul
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import mx.ma3r.tokyogoul.ui.theme.TokyoGoulTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            TokyoGoulTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+
+                }
+            }
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    TokyoGoulTheme {
+
+    }
+}
