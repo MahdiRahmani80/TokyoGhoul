ddf01af2faf7b570e8286ab7afebba9ad08b6d79
Mahdi Rahmani <mahdirahmani8@yahoo.com>
2023-08-12 23:06:58 +0330

    Add Firebase Analytis

--- app/src/main/java/mx/ma3r/tokyogoul/MainActivity.kt
+++ app/src/main/java/mx/ma3r/tokyogoul/MainActivity.kt
@@ -10,12 +10,21 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.google.firebase.analytics.FirebaseAnalytics
+import com.google.firebase.analytics.ktx.analytics
+import com.google.firebase.ktx.Firebase
 import mx.ma3r.tokyogoul.navigation.Navigation
 import mx.ma3r.tokyogoul.ui.theme.TokyoGoulTheme
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var analytics: FirebaseAnalytics
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        analytics = Firebase.analytics
+
         setContent {
             TokyoGoulTheme {
                 // A surface container using the 'background' color from the theme
